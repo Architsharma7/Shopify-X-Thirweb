757675b5501e3dbcc33192826899f35b80795eb1
Architsharma7 <architsharma7911@gmail.com>
2023-04-03 00:09:10 +0530

    feat: loyality point added

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import { NavigationMenu } from "@shopify/app-bridge-react";
-import Routes from "./Routes";
 
 import {
   AppBridgeProvider,
@@ -10,9 +8,6 @@ import {
 } from "./components";
 
 export default function App() {
-  // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
-  const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
 
   return (
     <PolarisProvider>
--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { Page, Layout } from "@shopify/polaris";
 
 import { NavigationSidebar } from "../components/Navigation";
 
 export default function HomePage() {
+  const navigate = useNavigate();
 
   return (
     <Page
-      title="ShopifyxThirdweb"
+      title="Tokengates"
+      primaryAction={{
+        content: "Create tokengate",
+        onAction: () => {
+          navigate("/createtokengate");
+        },
+      }}
     >
       <Layout>
         <Layout.Section>
